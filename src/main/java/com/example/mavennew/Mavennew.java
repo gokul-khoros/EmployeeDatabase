1741c7b2eed71f886bf92d6d8861ccc3810a8e59
Gokul_khoros <gokulreko@gmail.com>
2020-09-10 17:18:36 +0530

    main java file name changed

--- src/main/java/com/example/mavennew/Mavennew.java
+++ src/main/java/com/example/mavennew/Mavennew.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MavennewApplication {
+public class Mavennew {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(MavennewApplication.class, args);
+		SpringApplication.run(Mavennew.class, args);
 	}
-
 }
